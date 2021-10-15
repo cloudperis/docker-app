9f4945cdcaf818fef468e8ebad8cd04d724af0d6
cloudperis <cloudperis1.gmail.com>
2021-10-15 12:26:48 -0400

    update

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="Take Item"
+                    placeholder="See Item"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Take Item'}
+                        {submitting ? 'Adding...' : 'See Item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
